c2a2ceb3a66130c3ef4dcb5a27b63119f1d1a539
snts.abode <snts.abode@gmail.com>
2021-08-15 09:21:45 +0200

    addresses

---
+++ lib/addresses.ts
@@ -0,0 +1,13 @@
+export const Addresses = {
+  DYDX: {
+    MAINNET: {
+      ISoloMargin: '0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e',
+    },
+  },
+
+  ONEINCH: {
+    MAINNET: {
+      OneSplitAudit: '0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E',
+    }
+  }
+}
