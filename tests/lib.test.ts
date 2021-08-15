1705f0ab851a1c4c695da363e61ca19ebf4a9a77
snts.abode <snts.abode@gmail.com>
2021-08-15 20:19:57 +0200

    `lib` tests

--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -1,12 +1,13 @@
 /*
-mocha -r ts-node/register tests/lib.test.ts --timeout 900000
+yarn run mocha -r ts-node/register tests/lib.test.ts --timeout 900000
 */
 import * as lib from '../lib/lib'
 import { assert, expect } from 'chai'
+import BN from 'bn.js'
 
 describe('lib test suite', () => {
   it('Should call the getOneInchExpectedReturn function', async () => {
-    const oneInchExpectedReturn = await lib.getOneInchExpectedReturn(
+    const oneInchExpectedReturn = await lib.OneInch.getExpectedReturn(
       'DAI', 'USDC', lib.web3.utils.toWei('1')
     )
 
@@ -22,12 +23,12 @@ describe('lib test suite', () => {
   })
 
   it('Should call the fetch0xOrderBook function', async () => {
-    const OxOrderBook = await lib.fetch0xOrderBook('DAI', 'USDC')
+    const OxOrderBook = await lib.Ox.fetch0xOrderBook('DAI', 'USDC')
 
     expect(OxOrderBook).to.have.property('bids')
     expect(OxOrderBook).to.have.property('asks')
 
-    function dataAssertions(data: lib.I0xOrderBookData) {
+    function dataAssertions(data: lib.Ox.I0xOrderBookData) {
       expect(data).to.have.property('page')
       assert.isNumber(data.page)
       expect(data).to.have.property('perPage')
@@ -68,5 +69,35 @@ describe('lib test suite', () => {
 
     dataAssertions(OxOrderBook.asks)
     dataAssertions(OxOrderBook.bids)
+  })
+
+  it('Should call the Bot.calcProfitablity function', done => {
+    const profitabitlity_ = lib.Bot.calcProfitablity(
+      {
+        takerFeeAssetData: '0xf47261b00000000000000000000000006b175474e89094c44da98b954eedeac495271d0f',
+        takerFee: ''
+      } as unknown as lib.Ox.I0xOrderBookDataRecordOrder,
+      new BN('799520287800000000000000'),
+      new BN('799520297800000000000000'),
+      new BN('1700000'),
+      ['DAI', 'WETH']
+    )
+
+    const profitabitlity = parseFloat(lib.web3.utils.fromWei(
+      profitabitlity_.toString()
+    ))
+
+    const shouldbe = parseFloat('0.0099999999983')
+
+    assert.strictEqual(profitabitlity, shouldbe)
+
+    done()
+  })
+
+  it('Should call the Bot.callOrderBookAndFindArbs function', async () => {
+    // Promise should resolve.
+    // May or may not call the trade function, depends on if
+    // profitable arbitrage ops are found or not
+    await lib.Bot.callOrderBookAndFindArbs(['DAI', 'WETH'])
   })
 })
