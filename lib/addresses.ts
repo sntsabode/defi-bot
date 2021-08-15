f526d3ca26cfd277160eeb82e550a07e9a569fa5
snts.abode <snts.abode@gmail.com>
2021-08-15 15:05:05 +0200

    addresses

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,3 +1,23 @@
+const $: {
+  [sym in SupportedToken]: { MAINNET: string }
+} = {
+  USDC: {
+    MAINNET: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+  },
+
+  WETH: {
+    MAINNET: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+  },
+
+  DAI: {
+    MAINNET: '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+  },
+
+  SAI: {
+    MAINNET: '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359'
+  }
+}
+
 export const Addresses = {
   DYDX: {
     MAINNET: {
@@ -19,21 +39,11 @@ export const Addresses = {
     }
   },
 
-  $: {
-    USDC: {
-      MAINNET: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-    },
-
-    WETH: {
-      MAINNET: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    },
-
-    DAI: {
-      MAINNET: '0x6B175474E89094C44Da98b954EedeAC495271d0F'
-    },
-
-    SAI: {
-      MAINNET: '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359'
-    }
-  }
+  $
 }
+
+export type SupportedToken =
+  | 'USDC'
+  | 'WETH'
+  | 'DAI'
+  | 'SAI'
