c22258c44f7377e21e2124120893aec79f841dd5
snts.abode <snts.abode@gmail.com>
2021-08-15 17:05:00 +0200

    abis

--- lib/__abis__/abis.ts
+++ lib/__abis__/abis.ts
@@ -1,5 +1,10 @@
 /* eslint-disable */
 
 export const ONEINCH_ABIs = {
-  IOneSplitMulti: require('./abis/IOneSplitMulti.json'),
+  IOneSplitMulti: require('./abis/IOneSplitMulti.json')
+}
+
+export const ZRX_ABIs = {
+  ZRXExchange: require('./abis/ZRXExchange.json'),
+  FillOrderABI: require('./abis/FillOrderABI.json')
 }
