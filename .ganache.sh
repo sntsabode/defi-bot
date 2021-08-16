d7a86b683cf756a31d7bc2828d1b24549496cfaa
snts.abode <snts.abode@gmail.com>
2021-08-16 08:55:48 +0200

    boot ganache script

--- .ganache.sh
+++ .ganache.sh
@@ -6,10 +6,15 @@ do
   echo
 
   eth_node_url=$(<.eth_node_url)
+  dai_user_address=$(<.daiuseraddress)
 
-  echo "ETH_NODE_URL found:"
+  echo "ETH NODE URL found:"
   echo "$eth_node_url"
   echo
 
-  yarn run ganache-cli --fork "$eth_node_url" --port 7545
+  echo "DAI USER ADDRESS found:"
+  echo "$dai_user_address"
+  echo
+
+  yarn run ganache-cli --fork "$eth_node_url" --port 7545 --unlock 0x6b175474e89094c44da98b954eedeac495271d0f --unlock "$dai_user_address"
 done
